3101dbf4cb69b2634eaa13f97366248a44f2f044
VinhQuaan <hoangquan5824@gmail.com>
2025-05-29 18:49:44 +0700

    update create, index students

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layout')
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h2>Student Management</h2>
+    </div>
+    <div class="card-body">
+        <a href="{{ url('/students/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+        </a>
+        <br/>
+        <br/>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Mobile</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($students as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->address }}</td>
+                            <td>{{ $item->mobile }}</td>
+
+                            <td>
+                                <a href="{{ url('/students/' . $item->id) }}" title="View Student">
+                                    <button class="btn btn-info">
+                                        <i class="fa fa-eye"></i> View
+                                    </button>
+                                </a>
+                                <a href="{{ url('/students/' . $item->id) . '/edit' }}" title="Edit Student">
+                                    <button class="btn btn-info">
+                                        <i class="fa fa-edit"></i> Edit
+                                    </button>    
+                                </a>
+                                <form method="POST" action="{{ url('/students/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Student" onclick="return confirm('Are you sure you want to delete this student?')">
+                                        <i class="fa fa-trash"></i> Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach 
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
